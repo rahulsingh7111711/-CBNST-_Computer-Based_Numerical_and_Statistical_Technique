0be6340a44cf3894137cd4b99abfefd639b612a1
Rahul Kumar Singh <87928472+rahulsingh7111711@users.noreply.github.com>
2024-11-18 14:51:56 +0530

    Update Gauss_Elimination_Method.c

--- Gauss_Elimination_Method.c
+++ Gauss_Elimination_Method.c
@@ -3,7 +3,7 @@
 int main() {
     int i, j, k, n;
     float A[20][20], c, x[10], sum = 0.0;
-
+    printf("Name: RAHUL KUMAR SINGH \nRoll No: 48 \nSection: C\n\n");
     printf("Enter the order of matrix: ");
     scanf("%d", &n);
 
